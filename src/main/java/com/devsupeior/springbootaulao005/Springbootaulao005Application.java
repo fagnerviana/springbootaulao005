a6228e297a4723c81f878aa2e4d7fbb37aea4de9
fagnerviana <fagsantiago@gmail.com>
2022-06-16 23:06:35 -0300

    Inicializando o spring boot

---
+++ src/main/java/com/devsupeior/springbootaulao005/Springbootaulao005Application.java
@@ -0,0 +1,13 @@
+package com.devsupeior.springbootaulao005;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springbootaulao005Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springbootaulao005Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/devsupeior/springbootaulao005/Springbootaulao005ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsupeior.springbootaulao005;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springbootaulao005ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
